9ddfc46b84eca144bf069331756faf7163937266
jeanxwang <fazaqinthara629@gmail.com>
2023-11-15 14:36:19 +0700

    Tutorial 8 not finished yet

--- lib/widgets/shop_card.dart
+++ lib/widgets/shop_card.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_list/screens/shoplist_form.dart';
 import 'package:shopping_list/screens/menu.dart';
+import 'package:shopping_list/screens/list_product.dart';
 
 class ShopCard extends StatelessWidget {
   final ShopItem item;
@@ -21,13 +22,17 @@ class ShopCard extends StatelessWidget {
                 content: Text("Kamu telah menekan tombol ${item.name}!")));
 
           if (item.name == "Tambah Produk") {
-            // TODO: Gunakan Navigator.push untuk melakukan navigasi ke MaterialPageRoute yang mencakup ShopFormPage.
             Navigator.push(
               context,
               MaterialPageRoute(
                 builder: (context) => ShopFormPage(),
               ));
           }
+          else if (item.name == "Lihat Produk") {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => const ProductPage()));
+                }
+
         },
         child: Container(
           // Container untuk menyimpan Icon dan Text
